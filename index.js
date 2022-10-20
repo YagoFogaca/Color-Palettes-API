294ba057bbd4270c1726dce3fcdf9af039b6c4da
Yago Fogaca <yagofogaca20@gmail.com>
2022-10-20 18:41:55 -0300

    feat: palette crud implementation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { ConnectMongo } from './database/mongo/connection.js';
 import { factoryUser } from './factories/factory-user.js';
+import { factoryPalette } from './factories/factory-palettes.js';
 import express, { Router } from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
@@ -11,10 +12,12 @@ const port = process.env.PORT || 3000;
 const app = express();
 const router = Router();
 const user = factoryUser(router);
+const palette = factoryPalette(router);
 
 app.use(cors());
 app.use(express.json());
 app.use('/user', user.Routes());
+app.use('/palette', palette.Routes());
 
 app.listen(port, () => {
   console.log('Servidor rodando em: http://localhost:3000');

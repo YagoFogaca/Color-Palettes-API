a353e0f11dd9614a0d8ad6e7a85cfd71c67fcf4e
Yago Fogaca <yagofogaca20@gmail.com>
2022-10-20 18:39:31 -0300

    feat: creation of the palette factory

---
+++ factories/factory-palettes.js
@@ -0,0 +1,42 @@
+import { PaletteRepository } from '../database/repositorys/palette-repository.js';
+import { CreateUsecasePalette } from '../service/palette/usecase/create.js';
+import { DeleteUsecasePalette } from '../service/palette/usecase/delete.js';
+import { GetAllUsecasePalette } from '../service/palette/usecase/getAll.js';
+import { GetByIdUsecasePalette } from '../service/palette/usecase/getById.js';
+import { GetByIdUserUsecasePalette } from '../service/palette/usecase/getByIdUser.js';
+import { UpdateUsecasePalette } from '../service/palette/usecase/update.js';
+import { ServicePalette } from '../service/palette/service-palette.js';
+import { ControllerPalette } from '../controllers/controller-palette.js';
+import { ServicePalette } from '../service/palette/service-palette.js';
+import { RoutePalette } from '../routes/routes-palettes.js';
+import { PaletteEntity } from '../entities/palette/palette.entity.js';
+
+export function factoryPalette(router) {
+  const getAllUsecasePalette = new GetAllUsecasePalette(PaletteRepository);
+  const getByIdUsecasePalette = new GetByIdUsecasePalette(PaletteRepository);
+  const getByIdUserUsecasePalette = new GetByIdUserUsecasePalette(
+    PaletteRepository,
+  );
+  const createUsecasePalette = new CreateUsecasePalette(
+    PaletteRepository,
+    PaletteEntity,
+  );
+  const deleteUsecasePalette = new DeleteUsecasePalette(PaletteRepository);
+  const updateUsecasePalette = new UpdateUsecasePalette(
+    PaletteRepository,
+    getByIdUsecasePalette,
+  );
+
+  const service = new ServicePalette(
+    createUsecasePalette,
+    deleteUsecasePalette,
+    getAllUsecasePalette,
+    getByIdUserUsecasePalette,
+    updateUsecasePalette,
+  );
+
+  const controller = new ControllerPalette(service);
+  const routes = new RoutePalette(controller, router);
+
+  return routes;
+}
